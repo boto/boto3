a29fd724e74926a82bd4ceb71b78bf2c7f6295bc
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-09-15 12:40:37 -0700

    Use dummy data for tests instead of real services

--- tests/unit/test_resources.py
+++ tests/unit/test_resources.py
@@ -11,6 +11,9 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+import os
+
+import boto3.resources
 from boto3.resources import ServiceResource, ResourceFactory
 from tests import BaseTestCase
 
@@ -19,27 +22,31 @@ class TestResourceFactory(BaseTestCase):
     def setUp(self):
         super(TestResourceFactory, self).setUp()
         self.factory = ResourceFactory()
+        boto3.resources.RESOURCE_ROOT = os.path.join(
+            os.path.dirname(__file__),
+            'data'
+        )
 
     def test_get_service_returns_resource_class(self):
-        SQS = self.factory.create_class('sqs')
+        TestResource = self.factory.create_class('test')
 
-        self.assertIn(ServiceResource, SQS.__bases__,
+        self.assertIn(ServiceResource, TestResource.__bases__,
             'Did not return a ServiceResource subclass for service')
 
     def test_get_resource_returns_resource_class(self):
-        Queue = self.factory.create_class('sqs', 'Queue')
+        Queue = self.factory.create_class('test', 'Queue')
 
         self.assertIn(ServiceResource, Queue.__bases__,
             'Did not return a ServiceResource subclass for resource')
 
     def test_factory_sets_service_name(self):
-        Queue = self.factory.create_class('sqs', 'Queue')
+        Queue = self.factory.create_class('test', 'Queue')
 
-        self.assertEqual(Queue.service_name, 'sqs',
+        self.assertEqual(Queue.service_name, 'test',
             'Service name not set')
 
     def test_factory_sets_identifiers(self):
-        Message = self.factory.create_class('sqs', 'Message')
+        Message = self.factory.create_class('test', 'Message')
 
         self.assertTrue(hasattr(Message, 'identifiers'),
             'Class has no identifiers')
@@ -49,43 +56,43 @@ class TestResourceFactory(BaseTestCase):
             'Missing receipt_handle identifier from model')
 
     def test_factory_creates_dangling_resources(self):
-        SQS = self.factory.create_class('sqs')
+        TestResource = self.factory.create_class('test')
 
-        self.assertTrue(hasattr(SQS, 'Queue'),
+        self.assertTrue(hasattr(TestResource, 'Queue'),
             'Missing Queue class from model')
-        self.assertTrue(hasattr(SQS, 'Message'),
+        self.assertTrue(hasattr(TestResource, 'Message'),
             'Missing Message class from model')
 
     def test_can_instantiate_service_resource(self):
-        SQS = self.factory.create_class('sqs')
-        sqs = SQS()
+        TestResource = self.factory.create_class('test')
+        resource = TestResource()
 
-        self.assertIsInstance(sqs, ServiceResource,
+        self.assertIsInstance(resource, ServiceResource,
             'Object is not an instance of ServiceResource')
 
     def test_dangling_resources_create_resource_instance(self):
-        sqs = self.factory.create_class('sqs')()
-        q = sqs.Queue('test')
+        resource = self.factory.create_class('test')()
+        q = resource.Queue('test')
 
         self.assertIsInstance(q, ServiceResource,
             'Dangling resource instance not a ServiceResource')
 
     def test_dangling_resource_create_with_kwarg(self):
-        sqs = self.factory.create_class('sqs')()
-        q = sqs.Queue(url='test')
+        resource = self.factory.create_class('test')()
+        q = resource.Queue(url='test')
 
         self.assertIsInstance(q, ServiceResource,
             'Dangling resource created with kwargs is not a ServiceResource')
 
     def test_dangling_resource_shares_client(self):
-        sqs = self.factory.create_class('sqs')()
-        q = sqs.Queue('test')
+        resource = self.factory.create_class('test')()
+        q = resource.Queue('test')
 
-        self.assertEqual(sqs.client, q.client,
+        self.assertEqual(resource.client, q.client,
             'Client was not shared to dangling resource instance')
 
     def test_dangling_resource_requires_identifier(self):
-        sqs = self.factory.create_class('sqs')()
+        resource = self.factory.create_class('test')()
 
         with self.assertRaises(ValueError):
-            sqs.Queue()
+            resource.Queue()
