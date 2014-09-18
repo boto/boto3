ff504057223cd71f8ebbb7e7a53dc7982a9422a8
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-09-18 13:05:28 -0700

    Add stream logger config convenience function

--- boto3/__init__.py
+++ boto3/__init__.py
@@ -32,6 +32,32 @@ def setup_default_session(**kwargs):
     global DEFAULT_SESSION
     DEFAULT_SESSION = Session(**kwargs)
 
+def set_stream_logger(name='boto3', level=logging.DEBUG, format_string=None):
+    """
+    Add a stream handler for the given name and level to the logging module.
+    By default, this logs all boto3 messages to ``stdout``.
+
+        >>> import boto3
+        >>> boto3.set_stream_logger('boto3.resources', logging.INFO)
+
+    :type name: string
+    :param name: Log name
+    :type level: int
+    :param level: Logging level, e.g. ``logging.INFO``
+    :type format_string: str
+    :param format_string: Log message format
+    """
+    if format_string is None:
+        format_string = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 def _get_default_session():
     """
     Get the default session, creating one if needed.
