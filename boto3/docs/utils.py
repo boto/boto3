f0222f15f73ca5294253dd434c6c8d4e3aa6e325
kyleknap <kyleknap@amazon.com>
2015-06-19 15:22:04 -0700

    Update boto3 docs based on botocore doc updates

--- boto3/docs/utils.py
+++ boto3/docs/utils.py
@@ -79,11 +79,11 @@ class DocumentModifiedShape(object):
 
     def replace_documentation_for_matching_shape(self, event_name, section,
                                                  **kwargs):
-        if self._shape_name == section.meta.get('shape'):
+        if self._shape_name == section.context.get('shape'):
             self._replace_documentation(event_name, section)
         for section_name in section.available_sections:
             sub_section = section.get_section(section_name)
-            if self._shape_name == sub_section.meta.get('shape'):
+            if self._shape_name == sub_section.context.get('shape'):
                 self._replace_documentation(event_name, sub_section)
             else:
                 self.replace_documentation_for_matching_shape(
