06af21c518e6e11675699c34d873441e2e06b438
Felix Yan <felixonmars@archlinux.org>
2015-11-06 17:08:11 +0800

    Note explicit support for Python 3.5

--- setup.py
+++ setup.py
@@ -62,5 +62,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
