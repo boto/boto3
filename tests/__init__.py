af164b734ddcb087442b496c11be3e5360949d46
Ray Luo <rayluo@amazon.com>
2015-11-17 18:08:43 -0800

    Rename variable

--- tests/__init__.py
+++ tests/__init__.py
@@ -69,9 +69,9 @@ class BaseTestCase(unittest.TestCase):
         # We also need to patch the global default session.
         # Otherwise it could be a cached real session came from previous
         # "functional" or "integration" tests.
-        _patch_global_session = mock.patch('boto3.DEFAULT_SESSION')
-        _patch_global_session.start()
-        self.addCleanup(_patch_global_session.stop)
+        patch_global_session = mock.patch('boto3.DEFAULT_SESSION')
+        patch_global_session.start()
+        self.addCleanup(patch_global_session.stop)
 
     def tearDown(self):
         self.bc_session_patch.stop()
