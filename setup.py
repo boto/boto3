06bf852602a45a746682038085f07b41586d1355
Nate Prewitt <Nate.Prewitt@gmail.com>
2020-05-14 15:01:28 -0700

    update supported versions and run scripts

--- setup.py
+++ setup.py
@@ -55,5 +55,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
