ad4719c99a3213678b24077c94a5fb3d69ef3c50
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2020-09-11 18:08:32 +0000

    Bumping version to 1.14.60

--- boto3/__init__.py
+++ boto3/__init__.py
@@ -17,7 +17,7 @@ from boto3.session import Session
 
 
 __author__ = 'Amazon Web Services'
-__version__ = '1.14.59'
+__version__ = '1.14.60'
 
 
 # The default Boto3 session; autoloaded when needed.
