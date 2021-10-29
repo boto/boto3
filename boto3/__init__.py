4eb57d1a7a4196ccebe54f7ea1a7cc47e8d551d0
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2021-10-29 18:17:00 +0000

    Bumping version to 1.19.7

--- boto3/__init__.py
+++ boto3/__init__.py
@@ -18,7 +18,7 @@ from boto3.compat import _warn_deprecated_python
 
 
 __author__ = 'Amazon Web Services'
-__version__ = '1.19.6'
+__version__ = '1.19.7'
 
 
 # The default Boto3 session; autoloaded when needed.
