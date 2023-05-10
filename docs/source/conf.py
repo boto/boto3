6b3c3566560c09325ddf7376d58d5c4a89250093
jonathan343 <jonathangee09@gmail.com>
2023-05-10 15:17:46 -0700

    use  imported from botocore.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,12 @@ import os
 import boto3
 import boto3.session
 from boto3.docs import generate_docs
-from sphinx.locale import admonitionlabels
-from sphinx.writers.html5 import HTML5Translator as SphinxHTML5Translator
+
+try:
+    from botocore.docs import BotoHTML5Translator
+    CUSTOM_HTML_TRANSLATOR = BotoHTML5Translator
+except ImportError:
+    CUSTOM_HTML_TRANSLATOR = None
 
 
 session = boto3.session.Session(region_name='us-east-1')
@@ -300,48 +304,7 @@ texinfo_documents = [
 autoclass_content = 'both'
 
 
-class Boto3HTML5Translator(SphinxHTML5Translator):
-    """Extension of Sphinx's ``HTML5Translator`` for Boto3 documentation."""
-
-    STRONG_TO_H3_HEADINGS = [
-        "Example",
-        "Examples",
-        "Exceptions",
-        "Request Syntax",
-        "Response Structure",
-        "Response Syntax",
-        "Structure",
-        "Syntax",
-    ]
-
-    def visit_admonition(self, node, name=""):
-        """Uses the h3 tag for admonition titles instead of the p tag."""
-        self.body.append(self.starttag(node, "div", CLASS=("admonition " + name)))
-        if name:
-            title = f"<h3 class='admonition-title'> {admonitionlabels[name]}</h3>"
-            self.body.append(title)
-
-    def visit_strong(self, node):
-        """Visit a strong HTML element.
-        Opens the h3 tag for a specific set of words/phrases and opens the
-        strong tag for all others.
-        """
-        if len(node) > 0 and node[0] in self.STRONG_TO_H3_HEADINGS:
-            self.body.append(self.starttag(node, "h3", ""))
-        else:
-            self.body.append(self.starttag(node, "strong", ""))
-
-    def depart_strong(self, node):
-        """Depart a strong HTML element.
-        Closes the h3 tag for a specific set of words/phrases and closes the
-        strong tag for all others.
-        """
-        if node[0] in self.STRONG_TO_H3_HEADINGS:
-            self.body.append("</h3>")
-        else:
-            self.body.append("</strong>")
-
-
 def setup(app):
     # Register our custom HTML translator.
-    app.set_translator("html", Boto3HTML5Translator)
+    if CUSTOM_HTML_TRANSLATOR:
+        app.set_translator("html", CUSTOM_HTML_TRANSLATOR)
