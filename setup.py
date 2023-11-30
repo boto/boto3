e3f7ccf34feff62f4a9f5c20584c7ebdaba7a57e
Nate Prewitt <nate.prewitt@gmail.com>
2023-11-29 17:16:30 -0700

    Raise s3transfer floor to 0.8.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
 requires = [
     'botocore>=1.33.3,<1.34.0',
     'jmespath>=0.7.1,<2.0.0',
-    's3transfer>=0.8.0,<0.9.0',
+    's3transfer>=0.8.2,<0.9.0',
 ]
 
 
