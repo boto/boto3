8df5e9aa0d47f2d16e5bf555c31896cfaeb50036
Aarni Koskela <akx@iki.fi>
2024-08-07 10:14:35 +0300

    Use built-in `logging.NullHandler`
    Available since Python 3.1

--- boto3/__init__.py
+++ boto3/__init__.py
@@ -101,11 +101,6 @@ def resource(*args, **kwargs):
     return _get_default_session().resource(*args, **kwargs)
 
 
-# Set up logging to ``/dev/null`` like a library is supposed to.
+# Set up do-nothing logging like a library is supposed to.
 # https://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
-class NullHandler(logging.Handler):
-    def emit(self, record):
-        pass
-
-
-logging.getLogger('boto3').addHandler(NullHandler())
+logging.getLogger('boto3').addHandler(logging.NullHandler())
