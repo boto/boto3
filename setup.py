dc0f0a55b349a6f914579b2aae79e849749c026c
Nate Prewitt <nate.prewitt@gmail.com>
2025-04-22 12:41:29 -0700

    Deprecate Python 3.8 support (#4506)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     include_package_data=True,
     install_requires=requires,
     license="Apache License 2.0",
-    python_requires=">= 3.8",
+    python_requires=">= 3.9",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -47,7 +47,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
