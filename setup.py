1a8549fdee0c15d568984fd8fd7b9f8f8dee7afa
Nate Prewitt <nate.prewitt@gmail.com>
2025-07-16 10:39:30 -0600

    Add official Python 3.14 support with first release candidate

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
     ],
     project_urls={
         'Documentation': 'https://boto3.amazonaws.com/v1/documentation/api/latest/index.html',
