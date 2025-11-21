5fc9822f68cea01feb5b1725a8994e5551893ac1
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2025-11-21 19:16:55 +0000

    Bumping version to 1.41.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
 
 
 requires = [
-    'botocore>=1.41.1,<1.42.0',
+    'botocore>=1.41.2,<1.42.0',
     'jmespath>=0.7.1,<2.0.0',
     's3transfer>=0.15.0,<0.16.0',
 ]
